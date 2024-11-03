aa5273353ade38aca5f5a90cabe083d68cf7a71c
Falcury <falcury@gmail.com>
2024-11-03 19:21:40 +0100

    Menu stubs

---
+++ src/menu.c
@@ -0,0 +1,335 @@
+
+//49998
+void DO_NEW_MENUS() {
+    //stub
+}
+
+//49B7C
+void DO_OPTIONS_IN_GAME() {
+    //stub
+}
+
+//49E98
+void INIT_GENERAL_CHOICE() {
+    //stub
+}
+
+//4A0DC
+void AFFICHE_ECRAN_GENERAL() {
+    //stub
+}
+
+//4A178
+void DO_COMMANDE_GENERAL() {
+    //stub
+}
+
+//4A3D8
+void END_GENERAL_SCREEN() {
+    //stub
+}
+
+//4A44C
+void INIT_OPTIONS_CHOICE() {
+    //stub
+}
+
+//4A628
+void INIT_AFFICHE_ECRAN_OPTIONS() {
+    //stub
+}
+
+//4AB34
+void AFFICHE_ECRAN_OPTIONS() {
+    //stub
+}
+
+//4AF28
+void DO_COMMANDE_OPTIONS() {
+    //stub
+}
+
+//4B334
+void END_OPTIONS_SCREEN() {
+    //stub
+}
+
+//4B3EC
+void INIT_KEY_SCREEN() {
+    //stub
+}
+
+//4B514
+void INIT_AFFICHE_KEY_SCREEN() {
+    //stub
+}
+
+//4B8A4
+void AFFICHE_KEY_SCREEN() {
+    //stub
+}
+
+//4BACC
+void DO_COMMANDE_KEY() {
+    //stub
+}
+
+//4BD50
+void END_KEY_SCREEN() {
+    //stub
+}
+
+//4BDA4
+void INIT_BUTTON_SCREEN() {
+    //stub
+}
+
+//4BE04
+void INIT_AFFICHE_BUTTON_SCREEN() {
+    //stub
+}
+
+//4BF60
+void AFFICHE_BUTTON_JOYSTICK() {
+    //stub
+}
+
+//4C138
+void DO_BUTTON_COMMANDE() {
+    //stub
+}
+
+//4C390
+void END_BUTTON_COMMAND() {
+    //stub
+}
+
+//4C3D0
+void INIT_PAD_SCREEN() {
+    //stub
+}
+
+//4C4D0
+void INIT_AFFICHE_PAD_SCREEN() {
+    //stub
+}
+
+//4C854
+void AFFICHE_PAD_SCREEN() {
+    //stub
+}
+
+//4C9D8
+void DO_COMMANDE_PAD() {
+    //stub
+}
+
+//4CD38
+void END_PAD_SCREEN() {
+    //stub
+}
+
+//4CD94
+void InitParamGraphicsScreen() {
+    //stub
+}
+
+//4D094
+void INIT_GRAPHIC_SCREEN() {
+    //stub
+}
+
+//4D178
+void INIT_AFFICHE_GRAPHIC_SCREEN() {
+    //stub
+}
+
+//4D55C
+void AFFICHE_GRAPHIC_SCREEN() {
+    //stub
+}
+
+//4DA8C
+void DO_COMMANDE_GRAPHIC() {
+    //stub
+}
+
+//4DF88
+void END_GRAPHIC_SCREEN() {
+    //stub
+}
+
+//4E0A0
+void INIT_VIDEOMODE_SCREEN() {
+    //stub
+}
+
+//4E1CC
+void INIT_AFFICHE_VIDEOMODE_SCREEN() {
+    //stub
+}
+
+//4E470
+void AFFICHE_VIDEOMODE_SCREEN() {
+    //stub
+}
+
+//4E574
+void DO_COMMANDE_VIDEOMODE() {
+    //stub
+}
+
+//4E708
+void END_VIDEOMODE_SCREEN() {
+    //stub
+}
+
+//4E790
+void INIT_CALIBRATE_JOYSTICK() {
+    //stub
+}
+
+//4E804
+void AFFICHE_CALIBRATE_JOYSTICK() {
+    //stub
+}
+
+//4E99C
+void calibrate_joystick_prg() {
+    //stub
+}
+
+//4E9E8
+void AFFICHE_CENTER_JOYSTICK() {
+    //stub
+}
+
+//4EAE8
+void center_joystick_prg() {
+    //stub
+}
+
+//4EB28
+void MAIN_CALIBRATE_JOYSTICK() {
+    //stub
+}
+
+//4F17C
+void menu_prg() {
+    //stub
+}
+
+//4F2E0
+void DO_INTER_MENU() {
+    //stub
+}
+
+//4F318
+void DO_MENU() {
+    //stub
+}
+
+//4F488
+void default_key() {
+    //stub
+}
+
+//4F4EC
+void new_key() {
+    //stub
+}
+
+//4F50C
+void CalcSpeed() {
+    //stub
+}
+
+//4F698
+void general_init_screen() {
+    //stub
+}
+
+//4F76C
+void TestCompteur() {
+    //stub
+}
+
+//4F7B0
+void TestButtonReleased() {
+    //stub
+}
+
+//4F800
+void display_box_text_plasma() {
+    //stub
+}
+
+//4F878
+void display_box_text_fire() {
+    //stub
+}
+
+//4F8B4
+void display_box_msg_prg() {
+    //stub
+}
+
+//4F904
+void display_box_msg() {
+    //stub
+}
+
+//4F960
+void display_box_msg_commande() {
+    //stub
+}
+
+//4F9F0
+void AfficeYesNo() {
+    //stub
+}
+
+//4FAC4
+void DO_YESNOBIS() {
+    //stub
+}
+
+//4FB94
+void confirmation_msg() {
+    //stub
+}
+
+//4FEF0
+void SYNCHRO_LOOP_PALETTE() {
+    //stub
+}
+
+//4FF3C
+void apparition_prg() {
+    //stub
+}
+
+//500FC
+void fondu_prg() {
+    //stub
+}
+
+//501E8
+void change_couleur_prg() {
+    //stub
+}
+
+//502FC
+void FonduOption() {
+    //stub
+}
+
+//50A38
+void FonduPixel_prg() {
+    //stub
+}
+
+//50B0C
+void FonduPixel() {
+    //stub
+}
