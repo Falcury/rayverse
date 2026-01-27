14b080214fe032ad4309de976cc5e40af83f19cc
Falcury <falcury@gmail.com>
2026-01-27 18:16:34 +0100

    Implement fish (Eat at Joe's)

--- src/poisson.c
+++ src/poisson.c
@@ -1,21 +1,94 @@
 
 //67100
 void DO_POISSON_VERT_CMD(obj_t* obj) {
-    print_once("Not implemented: DO_POISSON_VERT_CMD"); //stub
+    // NOTE(Falcury): the PS1 and PC versions seem functionally identical.
+    DO_ONE_CMD(obj);
+    if (
+        (
+            !(obj->flags.flip_x) &&
+            on_block_chdir(obj, obj->offset_bx - 20, obj->offset_by - 3) &&
+            (s16) test_allowed(obj, obj->offset_bx - 20, obj->offset_by - 3)
+        ) ||
+        (
+            obj->flags.flip_x &&
+            on_block_chdir(obj, obj->offset_bx + 20, obj->offset_by - 3) &&
+            (s16) test_allowed(obj, obj->offset_bx + 20, obj->offset_by - 3)
+        )
+    ) {
+        if (obj->flags.flip_x) {
+            obj->flags.flip_x = 0;
+            skipToLabel(obj, 7, true);
+        } else {
+            obj->flags.flip_x = 1;
+            skipToLabel(obj, 8, true);
+        }
+    }
+    DO_OBJ_COLLISIONS(obj, 4);
 }
 
 //671F8
 void DO_POISSON_BLEU_CMD(obj_t* obj) {
-    print_once("Not implemented: DO_POISSON_BLEU_CMD"); //stub
+    // NOTE(Falcury): the PS1 and PC versions seem functionally identical.
+    DO_ONE_CMD(obj);
+    if (
+        (
+            !(obj->flags.flip_x) &&
+            on_block_chdir(obj, obj->offset_bx - 20, obj->offset_by - 3) &&
+            (s16) test_allowed(obj, obj->offset_bx - 20, obj->offset_by - 3)
+        ) ||
+        (
+            obj->flags.flip_x &&
+            on_block_chdir(obj, obj->offset_bx + 20, obj->offset_by - 3) &&
+            (s16) test_allowed(obj, obj->offset_bx + 20, obj->offset_by - 3)
+        )
+    ) {
+        if (obj->flags.flip_x) {
+            obj->flags.flip_x = 0;
+            skipToLabel(obj, 13, true);
+        } else {
+            obj->flags.flip_x = 1;
+            skipToLabel(obj, 14, true);
+        }
+        obj->y = obj->init_y;
+    }
+
+    if (obj->main_etat == 2 && obj->sub_etat == 17) {
+        if (obj->init_y <= obj->y) {
+            if ((obj->flags.flip_x))
+                skipToLabel(obj, 10, true);
+            else
+                skipToLabel(obj, 9, true);
+            obj->y = obj->init_y;
+        }
+    }
+    if (obj->main_etat == 2 && obj->sub_etat == 18) {
+        if (EOA(obj)) {
+            if (obj->speed_y == 5) {
+                obj->display_prio = 0;
+                obj->is_active = 0;
+            } else {
+                obj->speed_x = 0;
+                obj->speed_y = 5;
+                skipToLabel(obj, 12, true);
+            }
+        }
+    }
 }
 
 //67380
 void DoPoissonBleuPoingCollision(obj_t* obj, s16 sprite) {
-    print_once("Not implemented: DoPoissonBleuPoingCollision"); //stub
+    // NOTE(Falcury): PS1 implementation is located in DO_POING_COLLISION(). Seems to be functionally identical.
+    if (!(obj->main_etat == 2 && obj->sub_etat == 18)) {
+        skipToLabel(obj, 12, true);
+        obj->speed_x = 0;
+        obj->speed_y = 0;
+        obj->hit_points = 0;
+    }
 }
 
 //673B4
 bool can_free_fish(obj_t* fish) {
+    // NOTE(Falcury): the PS1 and PC versions seem functionally identical.
     bool result = true;
     for (s32 i = 0; i < level.nb_objects; ++i) {
         obj_t* cur_obj = level.objects + i;
@@ -30,7 +103,7 @@ bool can_free_fish(obj_t* fish) {
 
 //67410
 void DESACTIVE_FISH_COLLIS(obj_t* fish) {
-    /* 37CB8 8015C4B8 -O2 -msoft-float */
+    // NOTE(Falcury): the PS1 and PC versions seem functionally identical.
     u8 index = link_init[fish->id];
     obj_t* unk_obj = &level.objects[index];
 
@@ -51,7 +124,7 @@ void DESACTIVE_FISH_COLLIS(obj_t* fish) {
 
 //674C8
 void DO_PYRANHA(obj_t* in_obj) {
-    /* 37DA4 8015C5A4 -O2 -msoft-float */
+    // NOTE(Falcury): the PS1 and PC versions seem functionally identical.
     u8 can_free;
     s32 in_y_pos;
     s32 in_y;
@@ -117,6 +190,7 @@ void DO_PYRANHA(obj_t* in_obj) {
 
 //676EC
 void DoFishPoingCollision(obj_t* obj, s16 sprite) {
+    // NOTE(Falcury): PS1 implementation is located in DO_POING_COLLISION(). Seems to be functionally identical.
     obj_hurt(obj);
     if (obj->hit_points != 0) {
         obj->speed_x = 0;
@@ -131,6 +205,28 @@ void DoFishPoingCollision(obj_t* obj, s16 sprite) {
 
 //6773C
 void DoPoissonBleuRaymanZDD(obj_t* obj) {
-    print_once("Not implemented: DoPoissonBleuRaymanZDD"); //stub
+    // NOTE(Falcury): PS1 implementation is located in DO_RAY_IN_ZONE(). Seems to be functionally identical.
+    u8 sub_etat;
+    if (
+        !(
+            obj->main_etat == 2 &&
+            (obj->sub_etat == 16 || (sub_etat = obj->sub_etat, sub_etat == 17) || sub_etat == 18)
+        ) &&
+        (
+            ray.x + ray.offset_bx - obj->x - obj->offset_bx >= 0
+            ? obj->flags.flip_x
+            : !(obj->flags.flip_x)
+        )
+    ) {
+        if (obj->flags.flip_x)
+            obj->speed_x = 3;
+        else
+            obj->speed_x = -3;
+
+        obj->speed_y = -5;
+        set_main_etat(obj, 2);
+        set_sub_etat(obj, 16);
+        skipToLabel(obj, 11, true);
+    }
 }
 
